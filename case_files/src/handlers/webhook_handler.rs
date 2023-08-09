058d4722686bab66d62a53833a04e8fabe2ac20a
Leo Ngari <ngarimwenda@gmail.com>
2023-08-09 15:19:16 +0300

    created case internal code

--- case_files/src/handlers/webhook_handler.rs
+++ case_files/src/handlers/webhook_handler.rs
@@ -1,9 +1,11 @@
-use actix_web::{ post, HttpResponse, Responder, Error, web };
+use actix_web::{ post, web };
 
-use crate::models::update_model::Update;
+use crate::{ models::update_model::Update, internal::case::Case };
 
 #[post("/hook/updates")]
-pub async fn handle_updates_webhook(update: web::Json<Update>) -> Result<impl Responder, Error>{
-    let update = serde_json::to_string_pretty(&update).unwrap();
-    Ok(HttpResponse::Ok().json(update))
+pub async fn handle_updates_webhook(update: web::Json<Update>, case: web::Data<Case>) -> String {
+    let message = update.message.as_ref().unwrap();
+    let text = format!("Message is {}", message.text.as_ref().unwrap()).to_string();
+    println!("{text}");
+    text
 }
--- case_files/src/models/sticker_model.rs
+++ case_files/src/models/sticker_model.rs
@@ -6,7 +6,7 @@ use super::{ photo_size_model::PhotoSize, file_model::File };
 pub struct Sticker {
     file_id: String,
     file_unique_id: String,
-    #[serde(rename="type")]
+    #[serde(rename = "type")]
     _type: String,
     width: i32,
     height: i32,
@@ -16,12 +16,7 @@ pub struct Sticker {
     emoji: Option<String>,
     set_name: Option<String>,
     premium_animation: Option<File>,
-    #[serde(skip)]
-    mask_position: Option<MaskPosition>,
     custom_emoji_id: Option<String>,
     needs_repainting: Option<bool>,
     file_size: Option<i32>, // in bytes
 }
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct MaskPosition {}
